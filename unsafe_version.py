22fbafd8991de6f0b318c95f2d6cb6cdcb231975
Dhaniesh Murugan <dhaniesh@Dhanieshs-MacBook-Air.local>
2025-12-30 19:00:28 +0530

    add worker function for threads to access

---
+++ unsafe_version.py
@@ -0,0 +1,35 @@
+import threading
+import time
+
+class SharedCounter:
+    def __init__(self):
+        self.value = 0
+        self._lock = threading.Lock()
+
+    def increment(self):
+        temp = self.value
+        time.sleep(0)
+        self.value = temp + 1
+
+
+counter = SharedCounter()
+ITERATIONS = 10000
+THREADS = 5
+
+def worker():
+    for _ in range(ITERATIONS):
+        counter.increment()
+
+threads = []
+
+for _ in range(THREADS):
+    t = threading.Thread(target=worker)
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+
+expected = ITERATIONS * THREADS
+print("Expected:", expected)
+print("Actual:  ", counter.value)
